dd8e8042a9c0b83e52473bf67d4821c7b18a122e
Lucky Kim <lucky.hw.kim@gmail.com>
2022-05-04 07:39:37 +0000

    fix order page with table

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,10 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-  end
+    @line_items = LineItem.where(order_id: @order.id)
 
+  end
+  
   def create
     charge = perform_stripe_charge
     order  = create_order(charge)
