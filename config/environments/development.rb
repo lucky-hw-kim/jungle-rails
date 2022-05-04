b1891b039d36e0bc8b490c74de605d8e5cd28c23
Lucky Kim <lucky.hw.kim@gmail.com>
2022-05-04 00:21:39 +0000

    fix an issue with money formatting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.file_watcher = ActiveSupport::FileUpdateChecker
+  config.reload_classes_only_on_change = true
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
