a19d40657ca2efec58663b173d0b05e3bcf20c52
Lucky Kim <lucky.hw.kim@gmail.com>
2022-05-11 02:59:06 +0000

    Add shopping cart integration test

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,21 @@
+describe('Home page', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("when Add button is clicked, cart count changes", () => {
+    cy.contains("li.nav-item.end-0 > a", "My Cart (0)")
+    cy.get(':nth-child(1) > div > .button_to > .btn').click({ force: true });
+    cy.contains("li.nav-item.end-0 > a", "My Cart (1)")
+  })
+
+})
